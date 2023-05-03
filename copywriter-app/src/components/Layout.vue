d76fe24a171ec1bfb417beb0a9101953bf9a0760
Raja Nazirul <rajanazirul93@gmail.com>
2023-05-03 19:08:41 +0800

    add delete todo

--- copywriter-app/src/components/Layout.vue
+++ copywriter-app/src/components/Layout.vue
@@ -1,21 +1,3 @@
 <template>
-    <v-card>
-      <v-layout>
-        <v-app-bar color="surface-variant" title="Application bar"></v-app-bar>
-  
-        <v-navigation-drawer color="surface">
-          <v-list>
-            <v-list-item title="Drawer left"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-  
-        <v-navigation-drawer color="surface" location="right">
-          <v-list>
-            <v-list-item title="Drawer right"></v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-  
-        <v-main style="min-height: 300px;"></v-main>
-      </v-layout>
-    </v-card>
+    <v-text-field label="Label"></v-text-field>
   </template>
--- copywriter-app/src/views/HomeView.vue
+++ copywriter-app/src/views/HomeView.vue
@@ -1,9 +1,58 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+
+const todos = ref([])
+
+const newToDoContent = ref('')
+
+const addTodo = () => {
+  const newToDo = {
+    id: uuidv4(),
+    content: newToDoContent.value,
+    done: false
+  }
+  todos.value.unshift(newToDo)
+  newToDoContent.value = ''
+}
+
+const deleteTodo = id => {
+  todos.value = todos.value.filter(todo => todo.id !== id)
+}
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="columns">
+      <v-form @submit.prevent="addTodo">
+        <v-text-field label="Label" v-model="newToDoContent"></v-text-field>
+        <v-btn type="submit" :disabled="!newToDoContent">
+          Add
+        </v-btn>
+
+      </v-form>
+    </div>
+
+    <div v-for="todo in todos" class="d-flex align-center flex-column" :class="{ 'has-background-success-light': todo.done}">
+
+      <div class="text-subtitle-2">{{ todo.content }}</div>
+
+      <div>
+        <v-card width="400" :title=todo.content>
+          <v-col cols="auto">
+            <v-btn @click="deleteTodo(todo.id)" icon="mdi-plus"></v-btn>
+            <v-btn icon="mdi-plus"></v-btn>
+          </v-col>
+        </v-card>
+      </div>
+
+
+    </div>
+
   </main>
 </template>
+
+
+<style scoped></style>
