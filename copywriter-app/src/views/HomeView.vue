910b765fe4b4c97187b8ffcef61d89165fe1f9b5
Raja Nazirul <rajanazirul93@gmail.com>
2023-05-03 19:18:04 +0800

    add tailwind css

--- copywriter-app/src/views/HomeView.vue
+++ copywriter-app/src/views/HomeView.vue
@@ -35,19 +35,32 @@ const deleteTodo = id => {
       </v-form>
     </div>
 
-    <div v-for="todo in todos" class="d-flex align-center flex-column" :class="{ 'has-background-success-light': todo.done}">
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12" src="@/assets/logo.svg" alt="ChitChat Logo">
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
+
+    <div v-for="todo in todos" class="d-flex align-center flex-column" :class="{ 'has-background-success-light': to }">
 
       <div class="text-subtitle-2">{{ todo.content }}</div>
 
       <div>
         <v-card width="400" :title=todo.content>
           <v-col cols="auto">
-            <v-btn @click="deleteTodo(todo.id)" icon="mdi-plus"></v-btn>
-            <v-btn icon="mdi-plus"></v-btn>
+            <button type="button"
+              @click=""
+              class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Done</button>
+            <button type="button"
+              @click="deleteTodo(todo.id)"
+              class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Remove</button>
           </v-col>
         </v-card>
       </div>
-
 
     </div>
 
