f214e5b9f409cad14141ef68e53be706a7dc544c
Raja Nazirul <rajanazirul93@gmail.com>
2023-05-04 17:49:02 +0800

    test pinia

--- copywriter-app/src/stores/counter.ts
+++ copywriter-app/src/stores/counter.ts
@@ -1,12 +1,23 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useCounterStore = defineStore({
+  id: 'counter',
+  state: () => ({
+    count: 0
+  }),
+  getters: {
+    oddOrEven: (state) => {
+      if (state.count % 2 === 0) return 'even'
+      return 'odd'
+    }
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
   }
-
-  return { count, doubleCount, increment }
+  
 })
