df190da78e93154646a2319e462bb82df9711fdc
Raja Nazirul <rajanazirul93@gmail.com>
2023-05-04 18:37:35 +0800

    combine content properly

--- copywriter-app/src/stores/copywriting.ts
+++ copywriter-app/src/stores/copywriting.ts
@@ -1,25 +1,25 @@
 import { defineStore } from 'pinia'
 
 export const useCopywritingStore = defineStore({
-  id: 'copywriting',
-  state: () => ({
-    attention: '',
-    interest: '',
-    desire: '',
-    action: '',
-    content: ''
-  }),
-  getters: {
-    copywritingContent: (state) => {
-      return state.attention + state.interest + state.desire + state.action
-    }
-  },
-  actions: {
-    validateCopywriting() {
-        if ((this.attention == '') || (this.interest == '') || (this.desire == '') || (this.action == '')){
-            return false
+    id: 'copywriting',
+    state: () => ({
+        attention: '',
+        interest: '',
+        desire: '',
+        action: '',
+        content: ''
+    }),
+    getters: {
+        copywritingContent: (state) => {
+            return state.attention + '\n' + state.interest + '\n' + state.desire + '\n' + state.action
         }
-        return true
     },
-  }
+    actions: {
+        validateCopywriting() {
+            if ((this.attention == '') || (this.interest == '') || (this.desire == '') || (this.action == '')) {
+                return false
+            }
+            return true
+        },
+    }
 })
