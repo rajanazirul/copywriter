a63841e6150bfb106bf016cf57d9c2e1a2c7ba83
Raja Nazirul <rajanazirul93@gmail.com>
2023-05-05 20:57:25 +0800

    fix icon show

--- copywriter-app/src/main.ts
+++ copywriter-app/src/main.ts
@@ -8,13 +8,22 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 const app = createApp(App)
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
 })
 
 app.use(createPinia())
